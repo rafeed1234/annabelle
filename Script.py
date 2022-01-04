2c9ad7aeead2cdcfbe8dd9f272fe9546c0637d15
Jikku <66976170+jikkubot@users.noreply.github.com>
2022-01-04 09:52:53 +0400

    Update Script.py

--- Script.py
+++ Script.py
@@ -7,13 +7,13 @@ I have the following features. Tap the button in which you want help."""
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
+✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: CONTABO
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>Annabelle:</b>
-- Annabelle is a open source project made by <a href=https://t.me/vaishnavgr8>Vaishnav</a>. 
-- Source - <a href= https://github.com/Vaishnavofficial/Annabelle>Click here </a>
+- Annabelle is a open source project made by <a href=https://t.me/FilmClubChannel>Film Club</a>. 
+- Source - <a href= https://t.me/FilmClubChannel>Click here </a>
 <b>Support channel:</b>
-- <a href=https://t.me/Dianasupport>Team Diana</a>"""
+- <a href=https://t.me/FilmClubChannel>Team FilmClub</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and Annabelle will respond whenever a keyword is found the message
@@ -38,7 +38,7 @@ I have the following features. Tap the button in which you want help."""
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/Thugbots)</code>
+<code>[Button Text](buttonurl:https://t.me/FilmClubChannel)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message from Annabelle)</code>"""
