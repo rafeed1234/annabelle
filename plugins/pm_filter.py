f9da733a472a64eeb222e607018b2808f159f530
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2022-01-11 22:17:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -162,7 +162,9 @@ async def advantage_spoll_choker(bot, query):
         k = (movie, files, offset, total_results)
         await auto_filter(bot, query, k)
     else:
-        k = await query.message.edit('This Movie Not Found In DataBase')
+      k = await query.message.edit('This Movie Not Found In DataBase')
+      await asyncio.sleep(10)
+      await k.delete()
 
 
 @Client.on_callback_query()
